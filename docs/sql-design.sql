56de8175ee9055fc1332ae28d272c6275d41dc8c
memory98 <asdlkjh252567@gmail.com>
2023-01-19 19:28:23 +0900

    sql

---
+++ docs/sql-design.sql
@@ -0,0 +1,15 @@
+-- user 
+desc user;
+select * from user;
+-- join
+insert into user values(null,'jeongeunsung','a1@gmail.com',password('1234'),'male',now());
+
+-- login
+select no,name from user where email = 'a1@gmail.com' and password = password('1234');
+
+select no,name,email,password,gender from user where no = 1;
+
+
+update user set name='eunsung3',gender='female' where no = 2;
+update user set name='eunsung3', password = password('123'),gender='female' where no = 2;
+update user set name='eunsung2', password = password('1234'),gender='male' where no = 2;
